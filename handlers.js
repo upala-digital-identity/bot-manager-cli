29e60a009f60640764ee290f72b95cee6b486031
Petr Porobov <porobov.p@gmail.com>
2022-03-29 13:12:52 +0400

    init

---
+++ handlers.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const path = require('path')
+const { ethers, utils } = require('ethers')
+const { deployPool, attachToPool, PoolManager } = require('@upala/group-manager')
+
+// TODO 
+// check transaction mining for transaction commands (withdraw, setBaseScore)
+
+// SETUP AND INITIALIZATION 
+
+function getConfig() {
+    try {
+        return JSON.parse(fs.readFileSync('config.json'))
+    } catch {
+        return null
+    }
+}
+
+function saveNewConfig(newConfig) {
+    fs.writeFileSync('config.json', JSON.stringify(newConfig, null, 2))
+}
+
+function getWallet(config) {
+    const provider = new ethers.providers.JsonRpcProvider(config.ethNodeUrl)
+    return ethers.Wallet.fromMnemonic(config.mnemonic).connect(provider)
+}
+
+
+// HANDLERS
+
+function initHandler(config, network) {
+    if (config != null) { throw new Error("Config already exists")}
+
+    let networkID
+    if (network == '4' || network == "Rinkeby" || network == "Rinkeby") networkID = 4
+    if (network == '31337' || network == 'local') networkID = 31337
+
+    const initialConfig = {
+        chainId: networkID, // todo put rinkeby here (change to local for dev)
+        mnemonic: 'test test test test test test test test test test test junk',
+        ethNodeUrl: 'http://localhost:8545',  // use infura or alchemy here
+    }
+    fs.writeFileSync('config.json', JSON.stringify(initialConfig, null, 2))
+}
+
+async function registerHandler(config) {
+    if (config == null) { throw new Error('No config run \"init\" first.') }
+    console.log('Not inmplemented yet. Use explode command for now. It will take care for id registration')
+}
+
+async function listHandler(config) {
+    if (config == null) { throw new Error('No config run \"init\" first.') }
+}
+
+async function explodeHandler(config, poolAddress, score, bundleId, proof) {
+    if (config == null) { throw new Error('No config run \"init\" first.') }
+}
+
+module.exports = {
+    getConfig,
+    initHandler,
+    registerHandler,
+    listHandler,
+    explodeHandler
+}
---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+const program = new Command();
+const { 
+    getConfig,
+    initHandler,
+    registerHandler,
+    listHandler,
+    explodeHandler } = require('./handlers.js')
+
+const config = getConfig()
+
+program.version('0.0.1');
+
+program
+    .command('init')
+    .description('Creates config file and generates wallets')
+    .argument('<network>', 'Network by name (e.g. rinkeby)') // TODO
+    .action(function (network) {
+        initHandler(config, network);
+});
+
+program
+    .command('register')
+    .description('Register Upala id')
+    .action(async function () {
+        registerHandler(config)
+    })
+
+// todo list UpalaId
+// todo list rewards for a pool 
+program
+    .command('list')
+    .description('List accounts under control')
+    .action(async function () {
+        listHandler(config)
+    })
+
+// Will search for corresponding UpalaID (or will register one if necessary)
+// todo automatically pull score, bundleId, proof when graph or score explorer are ready
+program
+    .command('explode')
+    .description('Explode bot and get reward')
+    .argument('<poolAddress>', 'Pool address to attack')
+    .argument('<scoreAssignedTo>', 'Score bearing address')
+    .argument('<score>', 'Score assigned to the address')
+    .argument('<bundleId>', 'Bundle ID')
+    .argument('<proof>', 'Proof that address-score pair belongs to the Bundle ID')
+    .action(function (poolAddress, score, bundleId, proof) {
+        explodeHandler(config, poolAddress, score, bundleId, proof);
+    })
+
+program.parse(process.argv);
